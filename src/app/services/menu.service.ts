92bd14f8b110816fc5a4f699fc45c000e70ac3f2
yrrad01 <yrrad01@gmail.com>
2019-12-19 22:28:58 +0100

    menu.services.ts : ajout des fonctions coté admin

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class MenuService {
 
   constructor(private http: HttpClient, private router: Router) { }
+  
   findAllAvailableForToday(): Observable<any> {
     const url = 'http://localhost:8080/lunchtime/menu/findallavailablefortoday';
     return this.http.get(url, {responseType: 'json'})
@@ -20,6 +21,82 @@ export class MenuService {
        catchError(this.handleError<any>('findAllAvailableForToday')),
       );
   }
+
+
+  findAllavailableForWeek(weekNumber): Observable<any> {
+    const urlFindForWeek = 'http://localhost:8080/lunchtime/menu/find' +"/" +weekNumber;
+    return this.http.get(urlFindForWeek, {responseType: 'json'})
+     .pipe(
+       tap( menu => {
+         ;
+        }),
+       catchError(this.handleError<any>('findForWeek')),
+      );
+  }
+
+
+
+  find(menuId): Observable<any> {
+    const urlFind = 'http://localhost:8080/lunchtime/menu/find' +"/" +menuId;
+    return this.http.get(urlFind, {responseType: 'json'})
+     .pipe(
+       tap( menu => {
+         ;
+        }),
+       catchError(this.handleError<any>('find')),
+      );
+  }
+
+
+  findAll(): Observable<any> {
+    const urlFindAll = 'http://localhost:8080/lunchtime/menu/findAll';
+    return this.http.get(urlFindAll, {responseType: 'json'})
+     .pipe(
+       tap( menu => {
+         ;
+        }),
+       catchError(this.handleError<any>('findAll')),
+      );
+  }
+
+
+  deleteMenu(menuId): Observable<any> {
+    const deleteUrl = '"http://localhost:8080/lunchtime/menu/delete' + "/" +menuId;
+    return this.http.delete(deleteUrl, {responseType: 'json' })
+      .pipe(
+        tap(menu => {
+          ;
+        }),
+        catchError(this.handleError<any>('delete')),
+      );
+  }
+
+
+  addMenu(): Observable<any> {
+    const addUrl = '"http://localhost:8080/lunchtime/menu/add';
+    return this.http.delete(addUrl, {responseType: 'json' })
+      .pipe(
+        tap(menu => {
+          ;
+        }),
+        catchError(this.handleError<any>('add')),
+      );
+  }
+
+
+  updateMenu(menuId): Observable<any> {
+    const deleteUrl = '"http://localhost:8080/lunchtime/menu/update' + "/" +menuId;
+    return this.http.patch(deleteUrl,{responseType: 'json' })
+      .pipe(
+        tap(menu => {
+          ;
+        }),
+        catchError(this.handleError<any>('update')),
+      );
+  }
+
+
+
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
