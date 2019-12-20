86d849a1d25b8ca83ede1c272ba287aeff69101f
T0X <T0X@T0XMac.local>
2019-12-20 23:18:19 +0100

    order-detail

--- src/app/order-detail/order-detail.page.ts
+++ src/app/order-detail/order-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderService} from '../services/order.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-order-detail',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailPage implements OnInit {
 
-  constructor() { }
+  order;
+  key;
+  computedPrice;
 
-  ngOnInit() {
+  constructor(private orderService: OrderService, private route: ActivatedRoute) {
+    this.route.params
+        .subscribe(params => {this.key = params.id; console.log(params.id); });
   }
 
+  ngOnInit() {
+    this.getOrder(this.key);
+    this.computePrice(this.key, -1);
+  }
+
+  getOrder(orderId) {
+    this.orderService.getOrder(orderId)
+        .subscribe(
+            order => {console.log(order); this.order = order; },
+            (err) => console.log('Votre commande n\'a pas été trouvé !'),
+        );
+  }
+
+  computePrice(orderId, constraintId) {
+    this.orderService.computePrice(orderId, constraintId)
+        .subscribe(
+            order => {console.log(order); this.computedPrice = order.priceDF; },
+            (err) => console.log('Votre commande n\'a pas été trouvé')
+        );
+  }
 }
