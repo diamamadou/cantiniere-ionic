249f4319bd460e083d313cd89de3709256239fb8
T0X <T0X@T0XMac.local>
2019-12-30 20:00:02 +0100

    Interceptor Err0r

--- src/app/helpers/error-interceptor.ts
+++ src/app/helpers/error-interceptor.ts
@@ -4,22 +4,19 @@ import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import set = Reflect.set;
+import {Router} from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
       .pipe(
         catchError(err => {
-          if (err.status === 401) {
-              console.log('Error 401');
-            // this.authService.logOut();
-            // location.reload(true);
-          } else if (err.status === 403) {
-              console.log('helpp');
-              catchError(err);
+          if (err.status === 401 || err.status === 403) {
+              this.authService.logOut();
+              this.router.navigate(['/login']);
           }
           const error = err.message || err.statusText;
           return throwError(error);
